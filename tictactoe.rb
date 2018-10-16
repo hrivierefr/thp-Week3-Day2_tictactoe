6766ab3ca0c0521f3610e295c3b8b12520a8784c
hrivierefr <legsmower@gmail.com>
2018-10-16 14:10:31 +0200

    Finalisation : style, Premier joueur random

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,7 @@
+require 'colorize'
+require 'colorized_string'
+
+
 #Classe incluant les méthodes relatives au plateau (1 instance par jeu)
 class Board
 
@@ -17,42 +21,42 @@ class Board
 	#Affichage du plateau
 	def display_board
 		@grid.each_with_index {|boardcase, i|
+			print boardcase.print_case(i)
 			if i % 3 == 2
-				puts boardcase.value
-				print
-			else 
-				print "#{boardcase.value} | "
-			end				
+				puts " "
+			else
+				print "|"
+			end		
 		}
 	end
 
+	#Affichage du plateau à jouer
+	def display_empty_cases
+		@empty_cases.each_with_index {|value, i|
+			if value == i
+				print " #{i} "
+			else 
+				print "   "
+			end
+
+			if i % 3 == 2
+				puts " "
+			else
+				print "|"
+			end		
+		}
+	end
+
+	#Renvoie la valeur d'une case
 	def case_value(i)
 		@grid[i].value
 	end
 
 	def fill(i,sign)
 		@grid[i].fill(sign)
-		@empty_cases.delete(i)
+		@empty_cases[i]=" "
 	end
 
-	#Tableau des cases pouvant être jouées au prochain tour
-	def playable_grid
-		@grid.each_index {|i|
-			if i % 3 == 2
-				if @empty_cases.include?(i)
-					puts i
-				else 
-					puts " "
-				end
-			else 
-				if @empty_cases.include?(i)
-					print "#{i} | "
-				else 
-					print "  | "
-				end
-			end
-		}
-	end
 end
 
 #Classe incluant les méthodes relatives aux cases (9 instances par jeu)
@@ -62,32 +66,45 @@ class BoardCase
 		@value = value
 	end
 
+	#Modification de la valeur d'une case
 	def fill(sign)
 		@value = sign
 	end
+
+	#Coloration d'une case en fonction de sa valeur
+	def print_case(i)
+		if @value == "X"
+			return " #{@value} ".white.on_blue.bold
+		elsif @value == "O"
+			return " #{@value} ".white.on_red.bold
+		elsif @value == "_"
+			return " #{@value} ".light_black.on_white.bold
+		else
+			return " #{i} "
+		end
+	end
+				
 end
 
 #Classe incluant les méthodes relatives aux joueurs (2 instances par jeu)
 class Player
-	attr_accessor :name
+	attr_accessor :name, :score, :sign
 	def initialize
 		@name = name
+		@score = 0
 	end
 end
 
 #Classe décrivant chaque évènement du jeu
 class Game
 
-	attr_accessor :player1, :player2, :grid, :case0, :case1, :case2, :case3, :case4, :case5, :case6, :case7, :case8, :round, :sign, :case_played, :new_players
+	attr_accessor :player1, :player2, :grid, :case0, :case1, :case2, :case3, :case4, :case5, :case6, :case7, :case8, :round, :sign, :case_played, :new_players, :priority
 	
 	def initialize
-		@player1 = Player.new
-		@player2 = Player.new
-		@grid = Board.new
 		@new_players = true
 	end
 
-	#Méthode d'initialisation du plateau de jeu
+	#Initialisation du plateau de jeu
 	def game_load
 		@case0 = BoardCase.new
 		@case1 = BoardCase.new
@@ -108,20 +125,61 @@ class Game
 		@grid.add_case(@case6, 6)
 		@grid.add_case(@case7, 7)
 		@grid.add_case(@case8, 8)
-		@round = 1
 
 		puts ""
-		puts "C'est parti pour le tic-tac-toe de l'enfer !!"
-		puts ""
+		puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		print "!!!!!!!! " 
+		print "  C'est parti pour le tic-tac-toe de l'enfer  ".bold.red.on_white 
+		puts  " !!!!!!!!"
+		puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+		@round = 1
 	end
 
-	#Méthode de saisie des noms des joueurs
+	#Saisie des noms des joueurs
 	def players_names
+		
+		@player1 = Player.new
+		@player2 = Player.new
+
+		@player1.sign = "O"
+		@player2.sign = "X"
+
 		puts ""
-		print "Quel est le nom du joueur n°1 (O) ? > "
+		print "Quel est le nom du joueur n°1 (O) ? ".white.on_red
+		print " > " 
 		@player1.name = gets.chomp
-		print "Quel est le nom du joueur n°2 (X) ? > "
+		print "Quel est le nom du joueur n°2 (X) ? ".white.on_blue
+		print " > "
 		@player2.name = gets.chomp
+	end
+
+	#Premier joueur aléatoire
+	def rdm_start
+		puts ""
+		puts "Tirage au sort du joueur qui va débuter la partie :"
+		sleep(1)
+		15.times do
+			print "."
+			sleep(0.15)
+		end
+		print "Suspense"
+		15.times do
+			print "."
+			sleep(0.15)
+		end
+
+		@priority = rand(1)
+		puts""
+		sleep(1)
+		print "C'est "
+		if @priority == 0
+			print " #{player1.name} ".white.on_red.bold
+		else
+			print " #{player2.name} ".white.on_blue.bold
+		end
+		puts " qui commence."
+		sleep(2)
 	end
 
 	#Méthode pour évaluer s'il existe un vainqueur
@@ -148,21 +206,22 @@ class Game
 		end
 	end
 
-
-	#Méthode décrivant un tour de jeu
+	#Exécution d'un tour de jeu
 	def turn
-
-		@grid.display_board #Affichage du plateau
+		#Affichage du plateau
+		puts ""
+		@grid.display_board 
+		puts ""
 
 		#Détermination du joueur correspondant à ce tour de jeu
-		if @round.odd?
-			print player1.name 
-			@sign = "O"
+		if (@round + @priority).odd?
+			print " #{player1.name} ".white.on_red.bold
+			@sign = player1.sign
 		else
-			print player2.name
-			@sign = "X"
+			print " #{player2.name} ".white.on_blue.bold
+			@sign = player2.sign
 		end
-		puts ", à vous de jouer ! "
+		print ", à vous de jouer ! "
 
 		#Saisie du choix de jeu
 		play
@@ -174,10 +233,13 @@ class Game
 	#Choix de la case de jeu
 	def play
 		puts "Choisissez le numéro que vous souhaitez jouer parmi :"
-		@grid.playable_grid
+		@grid.display_empty_cases
 		print "> "
-		@case_played = gets.chomp.to_i
-		if @grid.empty_cases.include?(@case_played).!
+		@case_played = gets.chomp
+
+		if @case_played.between?("0","8") && @grid.empty_cases.include?(@case_played.to_i)
+			@case_played = case_played.to_i
+		else
 			puts "Saisie invalide"
 			play
 		end
@@ -186,7 +248,11 @@ class Game
 	#Cas du match nul
 	def draw
 		puts ""
-		print "Match nul. Voulez-vous rejouer pour vous départager (o/n) ? > "
+		@grid.display_board #Dernier affichage du plateau
+		puts ""
+		print "Match nul. Le score est toujours : "
+		print_score
+		print "Voulez-vous rejouer pour vous départager (o/n) ? > "
 		if gets.chomp == "o"
 			@new_players = false
 			perform
@@ -198,15 +264,25 @@ class Game
 	def winner_claim
 
 		@grid.display_board #Dernier affichage du plateau
-
+		puts ""
 		if @sign == "O"
-			print "Bravo #{@player1.name}"
+			print " Bravo #{@player1.name} ".white.on_red.bold
+			@player1.score += 1
 		elsif @sign == "X"
-			print "Bravo #{@player2.name}"
+			print " Bravo #{@player2.name} ".white.on_blue.bold
+			@player2.score += 1
 		end
 		puts ", vous avez gagné !"
-
+		print "Le score est maintenant : "
+		print_score
+		puts ""
 		new_game
+	end
+
+	def print_score
+		print " #{@player1.name} #{player1.score} ".white.on_red.bold
+		print " - "
+		puts  " #{player2.score} #{@player2.name} ".white.on_blue.bold
 	end
 
 	#Proposition d'un nouveau jeu
@@ -233,6 +309,8 @@ class Game
 			players_names
 		end
 		game_load
+
+		rdm_start
 
 		#Déroulement du jeu
 		while no_winner && @round < 10
